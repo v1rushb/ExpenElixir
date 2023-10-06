0713a8a3a4ae50d5756276809c2bfa2dad3e0c88
V1rushB <211111@ppu.edu.ps>
2023-10-06 18:07:15 +0300

    monitoring

--- main.ts
+++ main.ts
@@ -3,6 +3,10 @@ import express from 'express';
 const app = express();
 const PORT = 2077;
 
+app.get('/health',(req,res)=> {
+    res.status(200).send('Full HP');
+});
+
 app.get('/*',(req,res)=> { 
         res.status(404).send('Not Found');
 });
