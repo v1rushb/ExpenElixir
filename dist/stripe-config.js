1aa15be5ec278d3e42392568bcf8c3697bbe08b9
v1rushb <211111@ppu.edu.ps>
2023-10-21 15:07:42 +0300

    hotfix

---
+++ dist/stripe-config.js
@@ -0,0 +1,5 @@
+import Stripe from 'stripe';
+export const stripe = new Stripe('sk_test_51O1TW0EfItKzkbKdCuf5hIPvtZF1nz4gcdoaHrdKMunQbZJHLI4Bjxq23XyxIURS8KBSaiOrpq5n2uLWf0SsGN9G00eMO5iVPu', {
+    apiVersion: '2023-10-16',
+});
+//# sourceMappingURL=stripe-config.js.map
